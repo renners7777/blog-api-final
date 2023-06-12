6f4a1f23fe402d790fe15e3f81baa33105186bbe
Chris <104800006+renners7777@users.noreply.github.com>
2023-06-12 19:43:20 +0100

    First commit

---
+++ public/js/main.js
@@ -0,0 +1,71 @@
+const createPost = document.querySelector("#createPost");
+const updatePost = document.querySelectorAll(".updateBtn");
+const deletePost = document.querySelectorAll(".fa-trash");
+
+Array.from(post).forEach((element) => {
+  element.addEventListener("click", createPost);
+});
+
+async function createPost() {
+  const itemText = this.parentNode.childNodes[1].innerText;
+  try {
+    const response = await fetch("createPost", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        itemFromJS: itemText,
+      }),
+    });
+    const data = await response.json();
+    console.log(data);
+    location.reload();
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+Array.from(post).forEach((element) => {
+  element.addEventListener("click", updatePost);
+});
+
+async function updatePost() {
+  const itemText = this.parentNode.childNodes[1].innerText;
+  try {
+    const response = await fetch("updatePost", {
+      method: "put",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        itemFromJS: itemText,
+      }),
+    });
+    const data = await response.json();
+    console.log(data);
+    location.reload();
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+Array.from(post).forEach((element) => {
+  element.addEventListener("click", deletePost);
+});
+
+async function deletePost() {
+  const itemText = this.parentNode.childNodes[1].innerText;
+  try {
+    const response = await fetch("deletePost", {
+      method: "delete",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        itemFromJS: itemText,
+      }),
+    });
+    const data = await response.json();
+    console.log(data);
+    location.reload();
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+
---
+++ server.js
@@ -0,0 +1,75 @@
+const express = require('express')
+const app = express()
+const MongoClient = require('mongodb').MongoClient
+const PORT = 2121
+require('dotenv').config()
+
+
+let db,
+    dbConnectionStr = process.env.DB_STRING,
+    dbName = 'blog-api-db'
+
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+    .then(client => {
+        console.log(`Connected to ${dbName} Database`)
+        db = client.db(dbName)
+    })
+    
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+
+app.get('/',async (request, response)=>{
+    const getPosts = await db.collection('posts').find().toArray()
+    response.render('index.ejs', { title: 'title', posts: posts })
+    // db.collection('todos').find().toArray()
+    // .then(data => {
+    //     db.collection('todos').countDocuments({completed: false})
+    //     .then(itemsLeft => {
+    //         response.render('index.ejs', { items: data, left: itemsLeft })
+    //     })
+    // })
+    // .catch(error => console.error(error))
+})
+
+app.post('/addPost', (request, response) => {
+    db.collection('posts').insertOne({thing: request.body.postItem, completed: false})
+    .then(result => {
+        console.log('Post Added')
+        response.redirect('/')
+    })
+    .catch(error => console.error(error))
+})
+
+app.put('/updatePost', (request, response) => {
+    db.collection('posts').updateOne({itemToUpdate: request.body.itemFromJS},{
+        $set: {
+            completed: true
+          }
+    },{
+        sort: {_id: -1},
+        upsert: false
+    })
+    .then(result => {
+        console.log('Update Complete')
+        response.json('Update Complete')
+    })
+    .catch(error => console.error(error))
+
+})
+
+app.delete('/deletePost', (request, response) => {
+    db.collection('posts').deleteOne({itemToDelete: request.body.itemFromJS})
+    .then(result => {
+        console.log('Post Deleted')
+        response.json('Post Deleted')
+    })
+    .catch(error => console.error(error))
+
+})
+
+app.listen(process.env.PORT || PORT, ()=>{
+    console.log(`Server running on port ${PORT}`)
+})
