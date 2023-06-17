4dfb9b05d5dc1c67a258e7aa1849eed0324d6261
Chris <104800006+renners7777@users.noreply.github.com>
2023-06-17 21:28:43 +0100

    Updated server.js and index.ejs

--- public/js/main.js
+++ public/js/main.js
@@ -1,43 +1,47 @@
 const deleteBtn = document.querySelectorAll(".fa-trash");
 const item = document.querySelectorAll(".item span");
-const createPost = document.querySelector("#createPost");
-const updatePost = document.querySelectorAll(".updateBtn");
+const itemCompleted = document.querySelectorAll(".item span.completed");
 
+Array.from(deleteBtn).forEach((element) => {
+  element.addEventListener("click", deleteItem);
+}); //
 
-Array.from(post).forEach((element) => {
-  element.addEventListener("click", createPost);
+Array.from(item).forEach((element) => {
+  element.addEventListener("click", markComplete);
 });
 
-async function createPost() {
-  const itemText = this.parentNode.childNodes[1].innerText;
+Array.from(itemCompleted).forEach((element) => {
+  element.addEventListener("click", markUnComplete);
+});
+
+async function deleteItem() {
+  const itemText = this.parentNode.childNodes[1].innerText; //parentNode = li, childNode = span. From index.ejs. The innerText is whatever is added into the span. This function runs when the trash can (deleteBtn) is clicked.
   try {
-    const response = await fetch("createPost", {
-      method: "post",
-      headers: { "Content-Type": "application/json" },
+    const response = await fetch("deleteItem", {
+      method: "delete", //delete method is used to delete the item from the database.
+      headers: { "Content-Type": "application/json" }, //headers are used to tell the server what kind of data we are sending.
       body: JSON.stringify({
-        'itemFromJS': itemText,
+        // JSON.stringify() is used to convert the data into a string.
+        itemFromJS: itemText,
       }),
     });
-    const data = await response.json();
-    console.log(data);
-    location.reload();
+    const data = await response.json(); //await response.json waits from the response from the server
+    console.log(data); //console.log(data) prints the data in the console
+    location.reload(); //reloads(refreshes) the page
   } catch (err) {
-    console.log(err);
+    //if something goes wrong this code gives you an error message
+    console.log(err); //the error message is logged to the console.
   }
 }
 
-Array.from(post).forEach((element) => {
-  element.addEventListener("click", updatePost);
-});
-
-async function updatePost() {
+async function markComplete() {
   const itemText = this.parentNode.childNodes[1].innerText;
   try {
-    const response = await fetch("updatePost", {
+    const response = await fetch("markComplete", {
       method: "put",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
-        'itemFromJS': itemText,
+        itemFromJS: itemText,
       }),
     });
     const data = await response.json();
@@ -48,18 +52,14 @@ async function updatePost() {
   }
 }
 
-Array.from(post).forEach((element) => {
-  element.addEventListener("click", deletePost);
-});
-
-async function deletePost() {
+async function markUnComplete() {
   const itemText = this.parentNode.childNodes[1].innerText;
   try {
-    const response = await fetch("deletePost", {
-      method: "delete",
+    const response = await fetch("markUnComplete", {
+      method: "put",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
-        'itemFromJS': itemText,
+        itemFromJS: itemText,
       }),
     });
     const data = await response.json();
--- server.js
+++ server.js
@@ -1,75 +1,84 @@
-const express = require('express')
-const app = express()
-const MongoClient = require('mongodb').MongoClient
-const PORT = 2121
-require('dotenv').config()
-
+const express = require("express");
+const app = express();
+const MongoClient = require("mongodb").MongoClient;
+const PORT = 8080;
+require("dotenv").config();
 
 let db,
-    dbConnectionStr = process.env.DB_STRING,
-    dbName = 'blog-api-db'
+  dbConnectionStr = process.env.DB_STRING, // The dbConnectionStr is saved to the .env file. If you have a .env file, you need to npm install dotenv and require dotenv.
+  dbName = "blog-api-db"; // this is the name of the database that we will be using.
 
-MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
-    .then(client => {
-        console.log(`Connected to ${dbName} Database`)
-        db = client.db(dbName)
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true }).then(
+  (client) => {
+    console.log(`Connected to ${dbName} Database`);
+    db = client.db(dbName);
+  }
+);
+//The code above connects our server to the MongoDB database.
+
+app.set("view engine", "ejs"); // This line tells our app to use EJS as its view engine.
+app.use(express.static("public")); //This line tells our app to use the public folder for static files.
+app.use(express.urlencoded({ extended: true })); // This line tells our app to use the body-parser middleware. This is how we can get the data from the text.
+app.use(express.json()); //This line tells our app to use the body-parser middleware.
+
+app.get("/", async (request, response) => {
+  // const todoItems = await db.collection('todos').find().toArray()
+  // const itemsLeft = await db.collection('todos').countDocuments({completed: false})
+  // response.render('index.ejs', { items: todoItems, left: itemsLeft })
+  db.collection("posts")
+    .find()
+    .toArray()
+    .then((data) => {
+      db.collection("posts")
+          response.render("index.ejs", { items: data });
+          response.redirect("/");
+        })
+    .catch((error) => console.error(error));
     })
-    
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
 
-
-app.get('/',async (request, response)=>{
-    const getPosts = await db.collection('posts').find().toArray()
-    response.render('index.ejs', { items: blogPosts })
-    // db.collection('todos').find().toArray()
-    // .then(data => {
-    //     db.collection('todos').countDocuments({completed: false})
-    //     .then(itemsLeft => {
-    //         response.render('index.ejs', { items: data, left: itemsLeft })
-    //     })
-    // })
-    // .catch(error => console.error(error))
-})
-
-app.post('/addPost', (request, response) => {
-    db.collection('posts').insertOne({title: request.body.title, completed: true}, {content: request.body.content, completed: true})
-    .then(result => {
-        console.log('Post Added')
-        response.redirect('/')
+app.post("/addPost", (request, response) => {
+  db.collection("posts")
+    .insertOne({ thing: request.body.todoItem, completed: false })
+    .then((result) => {
+      console.log("Post Added");
+      response.redirect("/");
     })
-    .catch(error => console.error(error))
-})
+    .catch((error) => console.error(error));
+});
 
-app.put('/updatePost', (request, response) => {
-    db.collection('posts').updateOne({itemToUpdate: request.body.itemFromJS},{
+app.put("/updatePost", (request, response) => {
+  db.collection("posts")
+    .updateOne(
+      { thing: request.body.itemFromJS },
+      {
         $set: {
-            completed: true
-          }
-    },{
-        sort: {_id: -1},
-        upsert: false
+          completed: true,
+        },
+      },
+      {
+        sort: { _id: -1 },
+        upsert: false,
+      }
+    )
+    .then((result) => {
+      console.log("Post updated");
+      response.json("Post updated");
     })
-    .then(result => {
-        console.log('Update Complete')
-        response.json('Update Complete')
+    .catch((error) => console.error(error));
+});
+
+
+
+app.delete("/deletePost", (request, response) => {
+  db.collection("posts")
+    .deleteOne({ thing: request.body.itemFromJS })
+    .then((result) => {
+      console.log("Post Deleted");
+      response.json("Post Deleted");
     })
-    .catch(error => console.error(error))
+    .catch((error) => console.error(error));
+});
 
-})
-
-app.delete('/deletePost', (request, response) => {
-    db.collection('posts').deleteOne({itemToDelete: request.body.itemFromJS})
-    .then(result => {
-        console.log('Post Deleted')
-        response.json('Post Deleted')
-    })
-    .catch(error => console.error(error))
-
-})
-
-app.listen(process.env.PORT || PORT, ()=>{
-    console.log(`Server running on port ${PORT}`)
-})
+app.listen(process.env.PORT || PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
